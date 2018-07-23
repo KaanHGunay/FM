fe0604f32473110b1ef4ed44ace059c1b9e76b3b
KaanHGunay <kaangunay16@gmail.com>
2018-07-23 19:19:03 +0300

    Temel İşlevler Eklendi

---
+++ DataBase/Footballer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    class Footballer
+    {
+        public int ID { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public DateTime DogumTarihi { get; set; }
+    }
+}
